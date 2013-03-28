c5cfdbfceaf84592ce180fd5086f2ffbb1df3f89
Samuel Rodriguez <samus250@gmail.com>
2013-03-28 13:41:44 -0400

    Changed blink.c to main.c. Added header comments.

---
+++ Wavesphere/main.c
@@ -0,0 +1,27 @@
+/**
+ * main.c
+ * Author: Samuel
+ *
+ * This is the main entry point of the Wavesphere embedded application.
+ * Initial version contains blinking LED example.
+ * See System Flowchart.
+ */
+
+#include <msp430fr5969.h>
+
+int main(void) {
+	WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
+	P1DIR |= 0x01;					// Set P1.0 to output direction
+
+	for(;;) {
+		volatile unsigned int i;	// volatile to prevent optimization
+
+		P1OUT ^= 0x01;				// Toggle P1.0 using exclusive-OR
+
+		i = 10000;					// SW Delay
+		do i--;
+		while(i != 0);
+	}
+	
+	return 0;
+}
