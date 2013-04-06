613aa82df700b1cf3e758dbc9e694760e2b2d43a
danysantiago <daniel.santiago5@upr.edu>
2013-04-05 20:18:11 -0400

    Removed debug folders and modified git ignore

--- Testing Code/RTC SPI Test 5969/Debug/main.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-main.obj: ../main.c
-main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430fr5969.h
-main.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
-
-../main.c: 
-C:/ti/ccsv5/ccs_base/msp430/include/msp430fr5969.h: 
-C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
